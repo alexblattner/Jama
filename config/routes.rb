07f95d2e320390077b9b1f504ef719ca6a82289e
randomguy <alexblattnershalom@gmail.com>
2020-02-06 19:24:32 -0500

    basic html syntax
    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'main/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'main#index'
 end
