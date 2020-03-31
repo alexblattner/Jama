1c8cedb75924fe9e9f4da01f1cf3e3f9666ddb74
Jace Yang <35613800+jacethemagician@users.noreply.github.com>
2020-03-31 16:00:21 -0400

    test level

--- test/models/level_test.rb
+++ test/models/level_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
+#require 'minitest/autorun'
 
 class LevelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "the truth" do
+    assert true
+  end
+
+  # test "should not save level without name" do
+  #  level = Level.new
+  #  assert_not level.save, "Saved the level without name"
   # end
 end
