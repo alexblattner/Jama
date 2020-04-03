d58e7e673143aa4349bab305b752fa412be2295a
zors2ge00s2 <zors2ge00s2@gmail.com>
2020-04-03 14:58:44 -0400

    Added the level-doors route

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
   get 'main/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'main#index'
-  post 'levels/doors', to: 'levels#doors'
+  #post 'levels/doors', to: 'levels#doors'
+  get   'level-doors/:level', to: 'levels#doors'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
