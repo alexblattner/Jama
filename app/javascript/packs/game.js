7ee33588b21a937811bb120cdaebd77df4541175
randomguy <alexblattnershalom@gmail.com>
2020-05-12 21:30:09 -0400

    fixed searchbar issue

--- app/javascript/packs/game.js
+++ app/javascript/packs/game.js
@@ -3,5 +3,9 @@ $(document).on("keyup",'#search',function(){
 	$.get(url, function(d){
 		$(".tab-content").empty();
 		$(".tab-content").append(d);
+		if($("ul.nav.nav-tabs").children("li").eq(1).hasClass("active")){
+			$(".tab-content").find("#1").removeClass("active");
+			$(".tab-content").find("#2").addClass("active");
+		}
 	});
 });
