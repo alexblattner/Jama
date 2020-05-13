4f18accaf44265320c2d6aca7512553567fc0d3b
randomguy <alexblattnershalom@gmail.com>
2020-05-12 20:17:34 -0400

    p

--- app/serializers/door_serializer.rb
+++ app/serializers/door_serializer.rb
@@ -1,3 +1,3 @@
-class DoorSerializer < ActiveModel::Serializer
+class DoorSerializer < ActiveModel::Serializers
   attributes :id, :name, :next_levels, :description, :image, :result, :requirement
 end
